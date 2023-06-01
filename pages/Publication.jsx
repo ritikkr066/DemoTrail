aeb71a889c3367cfd588618ec32617c329bd95aa
Ritik <ritikkrsiwan345@gmail.com>
2023-06-01 20:06:42 +0530

    try again

--- pages/Publication.jsx
+++ pages/Publication.jsx
@@ -1,56 +1,56 @@
-// import React from 'react'
+import React from 'react'
 
-// const Publication = () => {
-//   return (
-//     <>
-//      <div className=' bg-[#E3D5CA] w-full  h-[100%]  '>
-//     <div className='flex items-center justify-center py-2 text-3xl font-semibold bg-[#E3D5CA]'>PUBLICATION</div> 
-//     <div className='px-2 py-2 text-2xl font-semibold'>Journal Publications</div> 
-//     <ol className='px-6 '>
-//         <li>⨠Abu T. Khan, Tasneem Parvin and Lokman H. Choudhury, "Silica supported perchloric acid (HClO4-SiO2): A versatile catalyst for the tetrahydropyranylation, thioacetalization and oxathioacetalization", Synthesis (2006, 2497-2502)</li>
-//         <li>⨠Abu T. Khan, Lokman H. Choudhury, Tasneem Parvin and Md. Asif Ali, "CeCl3⋅7H2O: An efficient and reusable catalyst for the preparation of β-acetamido carbonyl compounds by multi-component reactions (MCRs)", Tetrahedron Lett. (2006, 47, 8137-8141)</li>
-//         <li>⨠Abu T. Khan, Tasneem Parvin, Lokman H. Choudhury and Subrata Ghosh, "A simple and convenient synthetic protocol for oxidation of alkyl arenes into ketones using aqueous hydrogen bromide/hydrogen peroxide", Tetrahedron Lett. (2007, 48, 2271-2274)</li>
-//         <li>⨠Abu T. Khan, Tasneem Parvin and Lokman H. Choudhury, "Iron (III) chloride catalyzed convenient one-pot synthesis of β-acetamido carbonyl compounds", Tetrahedron (2007, 63, 5593-5601)</li>
-//         <li>⨠Abu T. Khan, Tasneem Parvin, Sarifuddin Gazi and Lokman H. Choudhury, "Bromodimethylsulfonium bromide mediated aza-Micheal addition of amines to electron deficient alkenes", Tetrahedron Lett. (2007, 48, 3805-3808)</li>
-//         <li>⨠Abu T. Khan, Tasneem Parvin, and Lokman H. Choudhury, "Bromodimethylsulfonium bromide (BDMS) catalyzed three-component Mannich-type reaction", Eur. J. Org. Chem. (2008, 834-839)</li>
-//         <li>⨠Abu T. Khan, Tasneem Parvin and Lokman H. Choudhury, "Effects of substituent in -position of 1, 3-Dicarbonyl Compounds in Bromodimethylsulfonium Bromide Catalyzed Multicomponent Reactions: A Facile Access to Functionalized Piperidines", J. Org. Chem. (2008, 73, 8398-8402)</li>
-//         <li>⨠Abu T. Khan, Tasneem Parvin and Lokman H. Choudhury, "A simple and convenient one-pot synthesis of benzimidazole derivatives using cobalt (II) chloride hexahydrate as catalyst", Synth. Comm. (2009, 39, 2339-2346)</li>
-//         <li>⨠Lokman H. Choudhury, Tasneem Parvin and Abu. T. Khan, "Recent advances in the application of bromodimethylsulfonium bromide (BDMS) in organic synthesis", Tetrahedron (2009, 65, 9513-9526)</li>
-//         <li>⨠Tasneem Parvin, "Methylacetoacetate: A Useful Reagent in Multicomponent Reactions", Synlett, Spotlight (2009, 16, 2713)</li>
-//         <li>⨠Lokman H. Choudhury and Tasneem Parvin, "Recent advances in the chemistry of imine-based multicomponent reactions (MCRs)", Tetrahedron (2011, 67, 8213-8228)</li>
-//         <li>⨠Suman Pal, Tasneem Parvin and Lokman H. Choudhury, "VCl3 catalyzed imine-based multicomponent reactions for the facile access of functionalized tetrahydropyridines and β-amino carbonyls", Molecular Diversity (2012, 16,129-143)</li>
-//         <li>⨠Md. Nasim Khan, Suman Pal, Tasneem Parvin and Lokman H. Choudhury, "A simple and efficient method for the facile access of highly functionalized pyridines and their fluorescence property studies", RSC Adv. (2012, 2, 12305-12314)</li>
-//         <li>⨠Suman Pal, Lokman H. Choudhury and Tasneem Parvin, "One-pot multicomponent reactions for the efficient synthesis of highly functionalized dihydropyridines ", Synthetic Communications (2013, 43, 986-992)</li>
-//         <li>⨠S. Karamthulla, S. Pal, Tasneem Parvin and L. H. Choudhury, "L-proline catalyzed multicomponent reactions: Facile access to 2H-benzo[g]pyrazolo[3,4-b]quinoline-5,10(4H,11H)-dione derivatives", RSC Advances (2014, 4, 15319-15324)</li>
-//         <li>⨠Ruchi Bharti and Tasneem Parvin , "One-pot Synthesis of Highly Functionalized Tetrahydropyridines: A Camphoresulfonic acid Catalyzed Multicomponent Reaction", Journal of Heterocyclic Chemistry (2015, 52, 1806-1811)</li>
-//         <li>⨠Ruchi Bharti and Tasneem Parvin, "Molecular diversity from the L-proline catalyzed three component reactions of 4-hydroxycoumarin, aldehyde and 3-aminopyrazole or 1, 3-dimethyl-6-aminouracil", Synthetic Communications (2015, 45, 1442-1450)</li>
-//         <li>⨠Ruchi Bharti and Tasneem Parvin, "Diversity oriented synthesis of tri-substituted methanes containing aminouracil and hydroxynaphthaquinone/ hydroxycoumarin moiety using organocatalysed multicomponent reactions in aqueous medium", RSC Advances (2015, 5, 66833-66839)</li>
-//         <li>⨠Ruchi Bharti and Tasneem Parvin, "Multicomponent synthesis of diverse pyrano fused benzophenazines using bifunctional thiourea based organocatalyst in aqueous medium", Molecular Diversity (2016, 20, 867-876)</li>
-//         <li>⨠Ruchi Bharti, Pooja Kumari, Tasneem Parvin and Lokman H. Choudhury, "Molecular diversity from the three-component reaction of 2-hydroxy-1,4-naphthaquinone, aldehydes and 6-aminouracils: a reaction condition dependent MCR", RSC Advances (2017, 7, 3928-3933)</li>
-//         <li>⨠A. Jana A.K. Panday, R. Mishra, Tasneem Parvin and L. H. Choudhury , "Synthesis of thio and selenoethers of cyclic β-hydroxy carbonyls and amino uracils: A metal-free regioselective I2/DMSO mediated reaction", Chemistry Select (2017, 9420-9424)</li>
-//         <li>⨠R. Bharti, P. Kumari, Tasneem Parvin and L. H. Choudhury, "Recent Advances of Aminopyrimidines in Multicomponent Reactions", Current Organic Chemistry (2018, 22, 417-445)</li>
-//         <li>⨠A. K. Panday, R. Mishra, A. Jana, Tasneem Parvin and L. H. Choudhury, "Synthesis of Pyrimidine Fused Quinolines by Ligand-Free Copper-Catalyzed Domino Reactions", Journal of Organic Chemistry (2018, 83, 3624-3632)</li>
-//         <li>⨠Pooja Kumari, Ruchi Bharti and Tasneem Parvin, "Synthesis of aminouracil tethered trisubstituted methanes in water by iodine catalyzed multicomponent reactions", Molecular Diversity (2019, 23, 205-213)</li>
-//         <li>⨠Pooja Kumari, Rahul Yadav, Ruchi Bharti and Tasneem Parvin, "Regioselective synthesis of pyrimidine fused tetrahydropyridines and pyridines by microwave-assisted one-pot reaction", Molecular Diversity ( 2020, 24, 107-117)</li>
-//         <li>⨠A. Mahata, P. Bhaumick, A. K. Panday, R. Yadav, Tasneem Parvin and L. H. Choudhury, "Multicomponent synthesis of diphenyl-1,3-thiazole-barbituric acid hybrids and their fluorescence property studies", New Journal of Chemistry (2020, 44, 4798-4811)</li>
-//         <li>⨠Tasneem Parvin, Rahul Yadav and L. H. Choudhury , "Recent applications of thiourea-based organocatalysts in asymmetric multicomponent reactions (AMCRs)", Organic & Biomolecular Chemistry (2020, 18, 5513-5532)</li>
-//         <li>⨠Rahul Yadav, Tasneem Parvin, A. K. Panday and L. H. Choudhury, "Synthesis of styryl linked fused dihydropyridines by catalyst-free multicomponent reactions", Molecular Diversity (2021, 25, 2161–2169)</li>
-//         <li>⨠Rahul Yadav and Tasneem Parvin, "Multicomponent synthesis of styryl linked benzo[h]pyrazolo[3,4-b]quinoline-5,6(10H)-diones by liquid assisted grinding", New Journal of Chemistry (2021, 45, 10388-10395)</li>
-//         <li>⨠D. Ali, Tasneem Parvin, and L. H. Choudhury, "Visible light-mediated C(sp2)–H selenylation of amino pyrazole and amino uracils in the presence of Rose Bengal as an organophotocatalyst", Journal of Organic Chemistry ( 2022, 87, 1230-1239)</li>
-//         <li>⨠Rahul Yadav, Darakshan, P. Bhaumick, L. H. Choudhury and Tasneem Parvin, "Synthesis of pentacyclic pyran fused pyrazolo benzo[h]quinolines by multicomponent reaction and their photophysical studies", ChemistrySelect (2022, 7, e202104124)</li>
-//         <li>⨠Rahul Yadav, Darakshan and Tasneem Parvin, "Catalyst-free one-pot synthesis of quinoline tethered α-amino ketones by multicomponent reactions", Journal of Heterocyclic Chemistry (2022, 59, 1935-1943)</li>
-//         <li>⨠Prabhas Bhaumick, Rohit Kumar, Swadhin S. Acharya, Tasneem Parvin and L. H. Choudhury, "Multicomponent synthesis of fluorescent thiazole-indole hybrids and thiazole based novel polymers", Journal of Organic Chemistry (2022, 87, 11399-11413)</li>
-//         <li>⨠Darakshan and Tasneem Parvin, "One-pot multicomponent synthesis of benzophenazine tethered tetrahydropyridopyrimidine derivatives", Molecular Diversity (2023, 27, 313-322)</li>
-//         <li>⨠R. Kumar, S. Acharya, P. Bhaumick, Tasneem Parvin, and L. H. Choudhury, "HFIP-mediated multi component reactions: Synthesis of fluorescent quinoline-fused pyrroles", Tetrahedron (2023, 132, 133250)</li>
-//         <li>⨠A. K. Panday, D. Ali, Tasneem Parvin and L. H. Choudhury* , "Metal-Free Synthesis of Pyrimidine and Naphthoquinone-Fused Pyrroles from Arylglyoxal-Based Domino Reactions,", ChemistrySelect (2023, 8, e202300158.)</li>
-//         <li>⨠Tasneem Parvin*, "Multicomponent reactions using C, N-binucleophilic nature of aminopyrazoles: Construction of pyrazole fused heterocycles. ", Topics in Current Chemistry (2023, Accepted)</li>
-//         <li>⨠Darakshan and Tasneem Parvin,*, "Domino reaction for the synthesis of pyrazole/isoxazole fused naphthyridine derivatives involving indole ring opening and double ring formation", Journal of Organic Chemistry ( 2023, Accepted)</li>
-//         <li>⨠Aatka Mehar and Tasneem Parvin,*, "Synthesis of anthracene linked polycyclic pyridine derivatives fused with coumarin and pyrazole by one-pot multicomponent strategy followed by oxidation, ", Journal of Heterocyclic Chemistry (2023, Under Revision)</li>
-//         <li>⨠Ujjain Chaurasia and Tasneem Parvin* , "Synthesis of coumarin-linked polycyclic fused pyrrole derivatives by HFIP mediated multicomponent reaction", Journal of Chemical Sciences ( 2023, Under Revision)</li>
-//     </ol>
-//     </div>
-//     </>
-//   )
-// }
+const Publication = () => {
+  return (
+    <>
+     {/* <div className=' bg-[#E3D5CA] w-full  h-[100%]  '>
+    <div className='flex items-center justify-center py-2 text-3xl font-semibold bg-[#E3D5CA]'>PUBLICATION</div> 
+    <div className='px-2 py-2 text-2xl font-semibold'>Journal Publications</div> 
+    <ol className='px-6 '>
+        <li>⨠Abu T. Khan, Tasneem Parvin and Lokman H. Choudhury, "Silica supported perchloric acid (HClO4-SiO2): A versatile catalyst for the tetrahydropyranylation, thioacetalization and oxathioacetalization", Synthesis (2006, 2497-2502)</li>
+        <li>⨠Abu T. Khan, Lokman H. Choudhury, Tasneem Parvin and Md. Asif Ali, "CeCl3⋅7H2O: An efficient and reusable catalyst for the preparation of β-acetamido carbonyl compounds by multi-component reactions (MCRs)", Tetrahedron Lett. (2006, 47, 8137-8141)</li>
+        <li>⨠Abu T. Khan, Tasneem Parvin, Lokman H. Choudhury and Subrata Ghosh, "A simple and convenient synthetic protocol for oxidation of alkyl arenes into ketones using aqueous hydrogen bromide/hydrogen peroxide", Tetrahedron Lett. (2007, 48, 2271-2274)</li>
+        <li>⨠Abu T. Khan, Tasneem Parvin and Lokman H. Choudhury, "Iron (III) chloride catalyzed convenient one-pot synthesis of β-acetamido carbonyl compounds", Tetrahedron (2007, 63, 5593-5601)</li>
+        <li>⨠Abu T. Khan, Tasneem Parvin, Sarifuddin Gazi and Lokman H. Choudhury, "Bromodimethylsulfonium bromide mediated aza-Micheal addition of amines to electron deficient alkenes", Tetrahedron Lett. (2007, 48, 3805-3808)</li>
+        <li>⨠Abu T. Khan, Tasneem Parvin, and Lokman H. Choudhury, "Bromodimethylsulfonium bromide (BDMS) catalyzed three-component Mannich-type reaction", Eur. J. Org. Chem. (2008, 834-839)</li>
+        <li>⨠Abu T. Khan, Tasneem Parvin and Lokman H. Choudhury, "Effects of substituent in -position of 1, 3-Dicarbonyl Compounds in Bromodimethylsulfonium Bromide Catalyzed Multicomponent Reactions: A Facile Access to Functionalized Piperidines", J. Org. Chem. (2008, 73, 8398-8402)</li>
+        <li>⨠Abu T. Khan, Tasneem Parvin and Lokman H. Choudhury, "A simple and convenient one-pot synthesis of benzimidazole derivatives using cobalt (II) chloride hexahydrate as catalyst", Synth. Comm. (2009, 39, 2339-2346)</li>
+        <li>⨠Lokman H. Choudhury, Tasneem Parvin and Abu. T. Khan, "Recent advances in the application of bromodimethylsulfonium bromide (BDMS) in organic synthesis", Tetrahedron (2009, 65, 9513-9526)</li>
+        <li>⨠Tasneem Parvin, "Methylacetoacetate: A Useful Reagent in Multicomponent Reactions", Synlett, Spotlight (2009, 16, 2713)</li>
+        <li>⨠Lokman H. Choudhury and Tasneem Parvin, "Recent advances in the chemistry of imine-based multicomponent reactions (MCRs)", Tetrahedron (2011, 67, 8213-8228)</li>
+        <li>⨠Suman Pal, Tasneem Parvin and Lokman H. Choudhury, "VCl3 catalyzed imine-based multicomponent reactions for the facile access of functionalized tetrahydropyridines and β-amino carbonyls", Molecular Diversity (2012, 16,129-143)</li>
+        <li>⨠Md. Nasim Khan, Suman Pal, Tasneem Parvin and Lokman H. Choudhury, "A simple and efficient method for the facile access of highly functionalized pyridines and their fluorescence property studies", RSC Adv. (2012, 2, 12305-12314)</li>
+        <li>⨠Suman Pal, Lokman H. Choudhury and Tasneem Parvin, "One-pot multicomponent reactions for the efficient synthesis of highly functionalized dihydropyridines ", Synthetic Communications (2013, 43, 986-992)</li>
+        <li>⨠S. Karamthulla, S. Pal, Tasneem Parvin and L. H. Choudhury, "L-proline catalyzed multicomponent reactions: Facile access to 2H-benzo[g]pyrazolo[3,4-b]quinoline-5,10(4H,11H)-dione derivatives", RSC Advances (2014, 4, 15319-15324)</li>
+        <li>⨠Ruchi Bharti and Tasneem Parvin , "One-pot Synthesis of Highly Functionalized Tetrahydropyridines: A Camphoresulfonic acid Catalyzed Multicomponent Reaction", Journal of Heterocyclic Chemistry (2015, 52, 1806-1811)</li>
+        <li>⨠Ruchi Bharti and Tasneem Parvin, "Molecular diversity from the L-proline catalyzed three component reactions of 4-hydroxycoumarin, aldehyde and 3-aminopyrazole or 1, 3-dimethyl-6-aminouracil", Synthetic Communications (2015, 45, 1442-1450)</li>
+        <li>⨠Ruchi Bharti and Tasneem Parvin, "Diversity oriented synthesis of tri-substituted methanes containing aminouracil and hydroxynaphthaquinone/ hydroxycoumarin moiety using organocatalysed multicomponent reactions in aqueous medium", RSC Advances (2015, 5, 66833-66839)</li>
+        <li>⨠Ruchi Bharti and Tasneem Parvin, "Multicomponent synthesis of diverse pyrano fused benzophenazines using bifunctional thiourea based organocatalyst in aqueous medium", Molecular Diversity (2016, 20, 867-876)</li>
+        <li>⨠Ruchi Bharti, Pooja Kumari, Tasneem Parvin and Lokman H. Choudhury, "Molecular diversity from the three-component reaction of 2-hydroxy-1,4-naphthaquinone, aldehydes and 6-aminouracils: a reaction condition dependent MCR", RSC Advances (2017, 7, 3928-3933)</li>
+        <li>⨠A. Jana A.K. Panday, R. Mishra, Tasneem Parvin and L. H. Choudhury , "Synthesis of thio and selenoethers of cyclic β-hydroxy carbonyls and amino uracils: A metal-free regioselective I2/DMSO mediated reaction", Chemistry Select (2017, 9420-9424)</li>
+        <li>⨠R. Bharti, P. Kumari, Tasneem Parvin and L. H. Choudhury, "Recent Advances of Aminopyrimidines in Multicomponent Reactions", Current Organic Chemistry (2018, 22, 417-445)</li>
+        <li>⨠A. K. Panday, R. Mishra, A. Jana, Tasneem Parvin and L. H. Choudhury, "Synthesis of Pyrimidine Fused Quinolines by Ligand-Free Copper-Catalyzed Domino Reactions", Journal of Organic Chemistry (2018, 83, 3624-3632)</li>
+        <li>⨠Pooja Kumari, Ruchi Bharti and Tasneem Parvin, "Synthesis of aminouracil tethered trisubstituted methanes in water by iodine catalyzed multicomponent reactions", Molecular Diversity (2019, 23, 205-213)</li>
+        <li>⨠Pooja Kumari, Rahul Yadav, Ruchi Bharti and Tasneem Parvin, "Regioselective synthesis of pyrimidine fused tetrahydropyridines and pyridines by microwave-assisted one-pot reaction", Molecular Diversity ( 2020, 24, 107-117)</li>
+        <li>⨠A. Mahata, P. Bhaumick, A. K. Panday, R. Yadav, Tasneem Parvin and L. H. Choudhury, "Multicomponent synthesis of diphenyl-1,3-thiazole-barbituric acid hybrids and their fluorescence property studies", New Journal of Chemistry (2020, 44, 4798-4811)</li>
+        <li>⨠Tasneem Parvin, Rahul Yadav and L. H. Choudhury , "Recent applications of thiourea-based organocatalysts in asymmetric multicomponent reactions (AMCRs)", Organic & Biomolecular Chemistry (2020, 18, 5513-5532)</li>
+        <li>⨠Rahul Yadav, Tasneem Parvin, A. K. Panday and L. H. Choudhury, "Synthesis of styryl linked fused dihydropyridines by catalyst-free multicomponent reactions", Molecular Diversity (2021, 25, 2161–2169)</li>
+        <li>⨠Rahul Yadav and Tasneem Parvin, "Multicomponent synthesis of styryl linked benzo[h]pyrazolo[3,4-b]quinoline-5,6(10H)-diones by liquid assisted grinding", New Journal of Chemistry (2021, 45, 10388-10395)</li>
+        <li>⨠D. Ali, Tasneem Parvin, and L. H. Choudhury, "Visible light-mediated C(sp2)–H selenylation of amino pyrazole and amino uracils in the presence of Rose Bengal as an organophotocatalyst", Journal of Organic Chemistry ( 2022, 87, 1230-1239)</li>
+        <li>⨠Rahul Yadav, Darakshan, P. Bhaumick, L. H. Choudhury and Tasneem Parvin, "Synthesis of pentacyclic pyran fused pyrazolo benzo[h]quinolines by multicomponent reaction and their photophysical studies", ChemistrySelect (2022, 7, e202104124)</li>
+        <li>⨠Rahul Yadav, Darakshan and Tasneem Parvin, "Catalyst-free one-pot synthesis of quinoline tethered α-amino ketones by multicomponent reactions", Journal of Heterocyclic Chemistry (2022, 59, 1935-1943)</li>
+        <li>⨠Prabhas Bhaumick, Rohit Kumar, Swadhin S. Acharya, Tasneem Parvin and L. H. Choudhury, "Multicomponent synthesis of fluorescent thiazole-indole hybrids and thiazole based novel polymers", Journal of Organic Chemistry (2022, 87, 11399-11413)</li>
+        <li>⨠Darakshan and Tasneem Parvin, "One-pot multicomponent synthesis of benzophenazine tethered tetrahydropyridopyrimidine derivatives", Molecular Diversity (2023, 27, 313-322)</li>
+        <li>⨠R. Kumar, S. Acharya, P. Bhaumick, Tasneem Parvin, and L. H. Choudhury, "HFIP-mediated multi component reactions: Synthesis of fluorescent quinoline-fused pyrroles", Tetrahedron (2023, 132, 133250)</li>
+        <li>⨠A. K. Panday, D. Ali, Tasneem Parvin and L. H. Choudhury* , "Metal-Free Synthesis of Pyrimidine and Naphthoquinone-Fused Pyrroles from Arylglyoxal-Based Domino Reactions,", ChemistrySelect (2023, 8, e202300158.)</li>
+        <li>⨠Tasneem Parvin*, "Multicomponent reactions using C, N-binucleophilic nature of aminopyrazoles: Construction of pyrazole fused heterocycles. ", Topics in Current Chemistry (2023, Accepted)</li>
+        <li>⨠Darakshan and Tasneem Parvin,*, "Domino reaction for the synthesis of pyrazole/isoxazole fused naphthyridine derivatives involving indole ring opening and double ring formation", Journal of Organic Chemistry ( 2023, Accepted)</li>
+        <li>⨠Aatka Mehar and Tasneem Parvin,*, "Synthesis of anthracene linked polycyclic pyridine derivatives fused with coumarin and pyrazole by one-pot multicomponent strategy followed by oxidation, ", Journal of Heterocyclic Chemistry (2023, Under Revision)</li>
+        <li>⨠Ujjain Chaurasia and Tasneem Parvin* , "Synthesis of coumarin-linked polycyclic fused pyrrole derivatives by HFIP mediated multicomponent reaction", Journal of Chemical Sciences ( 2023, Under Revision)</li>
+    </ol>
+    </div> */}
+    </>
+  )
+}
 
-// export default Publication
+export default Publication
